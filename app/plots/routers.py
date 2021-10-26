773212718f6d67c240138528444ce50376ca8623
yuanov <yuanov6@gmail.com>
2021-10-26 11:18:03 +0300

    parallel

--- app/plots/routers.py
+++ app/plots/routers.py
@@ -1,3 +1,5 @@
+import concurrent.futures
+import asyncio
 from fastapi import APIRouter, File, Depends
 
 from app.plots.enums import Shapes
@@ -9,5 +11,8 @@ router = APIRouter()
 
 
 @router.get('/rcs')
-async def rcs(z: float, shape: Shapes, user: User = Depends(AuthService.get_current_user)) -> File:
-    return rcs.get_plot(z=z, shape=shape)
+async def get_rcs(shape: Shapes, z: float = 1, user: User = Depends(AuthService.get_current_user)) -> File:
+    loop = asyncio.get_running_loop()
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        image = await loop.run_in_executor(pool, rcs.get_plot, z, shape)
+    return image
